a8d40bb6b5db5ebdb6245dda16847879aee2807d
MrHencke <hencke1@hotmail.com>
2020-09-23 15:40:11 +0200

    More changes

--- Oppg1-3/src/Oppgave2/Ansatt.java
+++ Oppg1-3/src/Oppgave2/Ansatt.java
@@ -4,64 +4,63 @@ import Oppgave2.Kjonn;
 
 public class Ansatt {
 
-	
-		public String fornavn;
-		public String etternavn;
-		public Kjonn kjonn;
-		public String stilling;
-		public int aarslonn;
+	public String fornavn;
+	public String etternavn;
+	public Kjonn kjonn;
+	public String stilling;
+	public int aarslonn;
 
-		public Ansatt(String fornavn, String etternavn, Kjonn kjonn, String stilling, int aarslonn) {
-			this.fornavn = fornavn;
-			this.etternavn = etternavn;
-			this.kjonn = kjonn;
-			this.stilling = stilling;
-			this.aarslonn = aarslonn;
-		}
+	public Ansatt(String fornavn, String etternavn, Kjonn kjonn, String stilling, int aarslonn) {
+		this.fornavn = fornavn;
+		this.etternavn = etternavn;
+		this.kjonn = kjonn;
+		this.stilling = stilling;
+		this.aarslonn = aarslonn;
+	}
 
-		@Override
-		public String toString() {
-			return "Navn: " + fornavn + " " + etternavn + ", " + "Kjønn: " + kjonn + ", "
-					+ "Stilling: " + stilling + ", Årslønn: " + aarslonn;
-		}
+	@Override
+	public String toString() {
+		return "Navn: " + fornavn + " " + etternavn + ", " + "Kjonn: " + kjonn + ", " + "Stilling: " + stilling
+				+ ", Aarslonn: " + aarslonn;
+	}
 
-		public String getFornavn() {
-			return fornavn;
-		}
+	public String getFornavn() {
+		return fornavn;
+	}
 
-		public void setFornavn(String fornavn) {
-			this.fornavn = fornavn;
-		}
+	public void setFornavn(String fornavn) {
+		this.fornavn = fornavn;
+	}
 
-		public String getEtternavn() {
-			return etternavn;
-		}
+	public String getEtternavn() {
+		return etternavn;
+	}
 
-		public void setEtternavn(String etternavn) {
-			this.etternavn = etternavn;
-		}
+	public void setEtternavn(String etternavn) {
+		this.etternavn = etternavn;
+	}
 
-		public Kjonn getKjonn() {
-			return kjonn;
-		}
+	public Kjonn getKjonn() {
+		return kjonn;
+	}
 
-		public void setKjonn(Kjonn kjonn) {
-			this.kjonn = kjonn;
-		}
+	public void setKjonn(Kjonn kjonn) {
+		this.kjonn = kjonn;
+	}
 
-		public String getStilling() {
-			return stilling;
-		}
+	public String getStilling() {
+		return stilling;
+	}
 
-		public void setStilling(String stilling) {
-			this.stilling = stilling;
-		}
+	public void setStilling(String stilling) {
+		this.stilling = stilling;
+	}
 
-		public int getAarslonn() {
-			return aarslonn;
-		}
+	public int getAarslonn() {
+		return aarslonn;
+	}
 
-		public int setAarslonn(int aarslonn) {
-			return this.aarslonn = aarslonn;
-		}
+	public int setAarslonn(int aarslonn) {
+		return this.aarslonn = aarslonn;
+	}
 }
